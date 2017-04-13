4bd2a9bfd47c333473c5142478f4a99bf3013bd3
John Snevily <snev7821@vandals.uidaho.edu>
2017-04-12 21:10:16 -0700

    More Street Scene stuff
    Added a (very) basic door mechanic, fixed the room slightly so the
    stairs are more accessible

--- SonarGraphicsTestProj/Assets/OpenSesame.cs
+++ SonarGraphicsTestProj/Assets/OpenSesame.cs
@@ -2,13 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OpenSesame : MonoBehaviour {
+public class OpenSesame : MonoBehaviour
+{
+    bool hasMoved;
 
-	OnTriggerEnter(Collider Other)
+
+    void Start()
+    {
+        hasMoved = false;
+    }
+
+	void OnTriggerEnter(Collider Other)
     {
         if (Other.tag == "Player")
         {
+            Debug.Log("OPENS");
+            Vector3 pos = gameObject.transform.position;
+            Vector3 newPos = new Vector3(-5, 0, 0);
+            if (hasMoved == false)
+            {
+                gameObject.transform.position = pos + newPos;
+                Invoke("Moved", 1);
+            }
 
+            if (hasMoved == true)
+            {
+                gameObject.transform.position = pos - newPos;
+                Invoke("MovedBack", 1);
+            }
+            
+            //Destroy(gameObject);
         }
+    }
+
+
+
+    void Moved()
+    {
+        hasMoved = true;
+    }
+
+    void MovedBack()
+    {
+        hasMoved = false;
     }
 }
