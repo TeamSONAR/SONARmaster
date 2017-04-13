b77c26802e8fbccd23dee749d9e61c176995bfe6
John Snevily <snev7821@vandals.uidaho.edu>
2017-04-12 20:27:06 -0700

    Updated Street Scene
    Cars are now realistic size. Added a first building.

---
+++ SonarGraphicsTestProj/Assets/OpenSesame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenSesame : MonoBehaviour {
+
+	OnTriggerEnter(Collider Other)
+    {
+        if (Other.tag == "Player")
+        {
+
+        }
+    }
+}
