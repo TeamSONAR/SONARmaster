20bd7177041aabf2a87aff8a2e629fb29a5ce512
John Snevily <snev7821@vandals.uidaho.edu>
2017-04-27 13:10:09 -0700

    Gave FPS controller collision
    Gave street scene fps controller collision, added prefab to player
    folder in prefabs folder

---
+++ SonarGraphicsTestProj/Assets/soundOnCollision.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class soundOnCollision : MonoBehaviour
+{
+    
+    public AudioClip jumpSound;
+    public AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        audioSource.clip = jumpSound;
+        audioSource.Play();
+
+    }
+}
