9747d8c7f6aa6a60a9f5e89c67b1f88a4821005f
dani2918 <mldaniel1525@yahoo.com>
2017-04-27 10:46:51 -0700

    Added scene to hear goal

--- SonarGraphicsTestProj/Assets/Scripts/SpawnCoinStatic.cs
+++ SonarGraphicsTestProj/Assets/Scripts/SpawnCoinStatic.cs
@@ -2,18 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnCoin : MonoBehaviour {
+public class SpawnCoinStatic : MonoBehaviour {
 	float x;
 	float y; 
 	float z;
 	Vector3 pos;
 	// Use this for initialization
 	void Start () {
-		x = Random.Range (0, 5);
-		y = 1.5f;
-		z = Random.Range (0, 5);
-		pos = new Vector3 (x*10, y, z*10);
-		transform.position = pos;
+		
 	}
 	
 	// Update is called once per frame
